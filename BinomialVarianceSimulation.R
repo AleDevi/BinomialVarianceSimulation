0616e8fa6b82f1b8b79e9c611a1a556593f1237c
Alessandro Devigili <alessandro.devigili@gmail.com>
2022-05-30 11:11:49 +0200

    Added a short (but time consuming) loop to test the time needed to perform the task in different situations (i.e. permutations number nad sample size)

--- BinomialVarianceSimulation.R
+++ BinomialVarianceSimulation.R
@@ -5,14 +5,14 @@ xx<-matrix(NA,100,3)
 xx[,1]<-rep(c(1:50),2)
 xx[,2]<-sample(70:3000, 100, replace=T)
 xx[,3]<-runif(100,0,1)
-
+xx<-as.data.frame(xx)
 
 ##CREATE A FUNCTION TO GENERATE A "SAMPLING DISTRIBUTION" OF LIVE/DEAD
 #SPERM BASED ON OBSERVED VIABILITY (WHITHIN MALE) and a given size of sampled 
 #samples (i.e. sampled cells):
 #
 #
-testrand<-function(data,size,repl,z,w){##data: the dataset, size:sample size (i.e. number of sperm), repl: iterazioni per maschio (numero di sottocampioni)
+testrand1<-function(data,size,repl,z,w){##data: the dataset, size:sample size (i.e. number of sperm), repl: iterazioni per maschio (numero di sottocampioni)
         samplingmeans<-matrix(NA,nrow(data),repl)#these are the proportions of the sampled sperm 
         samplingvariance<-NA
         yy<-matrix(NA,nrow=nrow(data),max(data,2))## max(data,2) to generate a matrix with a number of coums equal to the biggest number of perm collected
@@ -32,22 +32,29 @@ testrand<-function(data,size,repl,z,w){##data: the dataset, size:sample size (i.
 
 
 ########################
-testrand<-function(data,repl,prop,cells=0,size=max(data,cells)){##data: the dataset, repl: iterazioni per maschio (numero di ricampionamenti, prop=colonna in cui c'è la viability,cells, colonna in cui c'è il numero di spermi,size:sample size (i.e. number of sperm)
-        if(size==max(data,cells)){
+##data: the dataset, 
+#repl: iterazioni per maschio (numero di ricampionamenti), 
+#prop=colonna in cui c'è la viability,
+#cells, colonna in cui c'è il numero di spermi,
+#size:sample size (i.e. number of sperm),
+#K1,K2... are columns to keep
+
+testrand<-function(data,repl,prop,cells=1,size=max(data[,cells]),K1=1,K2=0,K3=0,K4=0){##data: the dataset, repl: iterazioni per maschio (numero di ricampionamenti, prop=colonna in cui c'è la viability,cells, colonna in cui c'è il numero di spermi,size:sample size (i.e. number of sperm),K1,K2... are columns to keep
+        if(size==max(data[,cells])){
                 samplingmeans<-matrix(NA,nrow(data),repl)#these are the proportions of the sampled sperm 
                 samplingvariance<-NA
-                yy<-matrix(NA,nrow=nrow(data),max(data,cells))## max(data,cells) to generate a matrix with a number of coums equal to the biggest number of perm collected
+                yy<-matrix(NA,nrow=nrow(data),max(data[,cells]))## max(data,cells) to generate a matrix with a number of coums equal to the biggest number of perm collected
                 celsampled<-NA
                 for(j in 1:repl){
                         for (i in 1:nrow(data)){
-                                yy[i,]<-c(rbinom(data[i,cells], 1, data[i,prop]),rep(NA,max(data,cells)-data[i,cells]))#yy[i,]<-rbinom(data[i,2], 1, data[i,3])### data[i,2] here is to generate a binomial variable based on the number of sperm observed. it was "size" to generate one based on a fixed sample
+                                yy[i,]<-c(rbinom(data[i,cells], 1, data[i,prop]),rep(NA,max(data[,cells])-data[i,cells]))#yy[i,]<-rbinom(data[i,2], 1, data[i,3])### data[i,2] here is to generate a binomial variable based on the number of sperm observed. it was "size" to generate one based on a fixed sample
                                 samplingmeans[,j]<-rowMeans(yy,na.rm =T) #in samplingmeans mi genera una serie di PROPORZIONI (n=repl) basate OGNI VOLTA su un numero di "spermi" uguale a quelli testati nel maschio. Sono le proporzioni simulate
                         }
                         celsampled<-rowSums(!is.na(yy))}
                 for (h in 1:nrow(samplingmeans)){
                         samplingvariance[h]<-var(samplingmeans[h,])
                 }
-                samplingmeans<-cbind(data[,c(1:3)],repl,celsampled,samplingvariance)#add ",samplingmeans[,c(1:5)]" after celsampled to also add the firs 5 sampled proportions
+                samplingmeans<-cbind(data[,c(K1,K2,K3,K4)],repl,celsampled,samplingvariance)#add ",samplingmeans[,c(1:5)]" after celsampled to also add the firs 5 sampled proportions
                 samplingmeans
         }
         else{
@@ -64,15 +71,15 @@ testrand<-function(data,repl,prop,cells=0,size=max(data,cells)){##data: the data
                 for (h in 1:nrow(samplingmeans)){
                         samplingvariance[h]<-var(samplingmeans[h,])
                 }
-                samplingmeans<-cbind(data[,c(1:3)],repl,celsampled,samplingvariance)#add ",samplingmeans[,c(1:5)]" after celsampled to also add the firs 5 sampled proportions
+                samplingmeans<-cbind(data[,c(K1,K2,K3,K4)],repl,celsampled,samplingvariance)#add ",samplingmeans[,c(1:5)]" after celsampled to also add the firs 5 sampled proportions
                 samplingmeans       
         }
 }
 
 
 #test the function
-testrand(xx,repl=50,3,cells)
-results<-as.data.frame(testrand(xx,repl=1000,prop=3,size=700))
+testrand(xx,repl=50,3)
+as.data.frame(testrand(xx,repl=100,prop=3,size=30))
 
 names(results)<-c("male","obssperm","obsprop","permutations","cells","variance")
 plot(results$obsprop,results$variance)
@@ -102,3 +109,27 @@ hist(results[,3])
 hist(results2[,1])
 hist(results2[,2])
 hist(results2[,3])
+
+
+
+dataset<-read.csv("C:/Users/Color&Sound/OneDrive/Desktop/sperm_phenotype.csv",
+                    header = T)
+dataset
+
+##data: the dataset, 
+##repl: iterazioni per maschio (numero di ricampionamenti, 
+##prop=colonna in cui c'è la viability,
+##cells, colonna in cui c'è il numero di spermi,
+##size:sample size (i.e. number of sperm)
+
+
+dataset2<-dataset[,c(1,10,11)]
+testrand(dataset,repl=100,prop=10,cells=11,K1=2,K2=3)
+str(dataset2)
+
+testrand(dataset,repl=100,prop=10,size=50,K1=2,K2=3)
+
+
+testrand(xx,repl=100,prop=3,cells=2,K1=1,K2=2)
+
+
